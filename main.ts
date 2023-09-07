35ade1597af9553ff06d98a91674885fbc6cdf77
Pascal Eberhard <35179162+paeber@users.noreply.github.com>
2023-09-07 16:06:38 +0200

    Add basic functions to read module

--- main.ts
+++ main.ts
@@ -1,27 +1,64 @@
 
-
+//% color="#ff78f1" icon="\uf3c5" block="GNSS DfRobot"
 namespace GNSS{
 
     const DEVICE_ADDR = 0x20
     
-    function readRegister(reg: GNSS_REG){
+    //%blockId=gnss_reg
+    //%block="Read GNSS Module Register"
+    export function readRegister(reg: GNSS_REG){
         pins.i2cWriteNumber(DEVICE_ADDR, reg, NumberFormat.Int8LE, true)
         let val = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, false)
         return val
     }
 
-    function getLongitude() {
-        pins.i2cWriteNumber(DEVICE_ADDR, GNSS_REG.I2C_LAT_X_24, NumberFormat.Int8LE, true)
-        let LAT_X_24 = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, false)
-        let LAT_X_16 = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, false)
-        let LAT_X_8 = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, false)
+    //%blockId=latitude
+    //%block="Latitude"
+    export function getLatitude() {
+        pins.i2cWriteNumber(DEVICE_ADDR, GNSS_REG.I2C_LAT_1, NumberFormat.Int8LE, true)
+        let LAT_1 = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, true)
+        let LAT_2 = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, false)
+        return LAT_1 + LAT_2 /100
     }
 
+    //%blockId=longitude
+    //%block="Longitude"
+    export function getLongitude() {
+        pins.i2cWriteNumber(DEVICE_ADDR, GNSS_REG.I2C_LON_1, NumberFormat.Int8LE, true)
+        let LON_1 = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, true)
+        let LON_2 = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, false)
+        return LON_1 + LON_2 / 100
+    }
+
+    //%blockId=altiutude
+    //%block="Altitude"
     export function getAltitude() {
         pins.i2cWriteNumber(DEVICE_ADDR, GNSS_REG.I2C_ALT_H, NumberFormat.Int8LE, true)
         let ALT_H = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, true)
         let ALT_L = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, false)
         return (ALT_H << 8) | ALT_L
     }
+
+    //%blockId=time
+    //%block="Time"
+    export function getTime() {
+        pins.i2cWriteNumber(DEVICE_ADDR, GNSS_REG.I2C_HOUR, NumberFormat.Int8LE, true)
+        let hh = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, true)
+        let mm = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, true)
+        let ss = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, false)
+        return hh+":"+mm+":"+ss
+    }
+
+    //%blockId=date
+    //%block="Date"
+    export function getDate() {
+        pins.i2cWriteNumber(DEVICE_ADDR, GNSS_REG.I2C_YEAR_H, NumberFormat.Int8LE, true)
+        let Y_H = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, true)
+        let Y_L = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, true)
+        let MM = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, true)
+        let DD = pins.i2cReadNumber(DEVICE_ADDR, NumberFormat.UInt8LE, false)
+        return ((Y_H << 8) | Y_L) + "/" + MM + "/" + DD
+    }
+
 
 }
